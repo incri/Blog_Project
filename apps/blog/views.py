e1e53d3647b90c283c436b802e6fe7711fcd90a1
Baibhav <paudelbaibhav0@gmail.com>
2022-04-17 22:34:44 +0545

    fixed git ignore

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,11 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from apps.blog.forms import PostForm
 from .models import Post
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import ListView, FormView, CreateView
+# from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
+    return render(
+        request,
+        'blog/home.html',
+        context={'posts': Post.objects.all()}
+    )
 
-    data = {
-        'posts': Post.objects.all()
-    }
 
-    return render(request, 'blog/home.html', context= data)
+def post_create(request):
+    if request.method.lower() == 'get':
+        return render(request, 'blog/post_create.html', {'form': form})
+    else:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home-page')
+
+"""
+class PostList(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+"""
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_create.html'
+    success_url = '/home/'
+    form_class = PostForm
--- apps/urls.py
+++ apps/urls.py
@@ -3,4 +3,7 @@ from .blog import views
 
 urlpatterns = [
     path('home/', views.home, name="home-page"),
+    # path('posts/', views.PostList.as_view(), name='post-list')
+    path('post/', views.post_create),
+    path('create/', views.PostCreateView.as_view())
 ]
