dac4f234faff6e2fd478bba2f976c3788b557098
Baibhav <paudelbaibhav0@gmail.com>
2022-05-28 23:23:39 +0545

    profile edit update

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserUpdateForm, ProfileUpdateForm
 
 
@@ -7,7 +7,14 @@ def ProfileViews(request):
 
 
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
-
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('profile-page')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     return render(request, 'user/profile.html', {'u_form': u_form, 'p_form': p_form})
